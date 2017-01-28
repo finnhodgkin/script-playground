82024dd26d772fd7dc7424437ab4679612d001cb
finnhodgkin <finnhodgkin@gmail.com>
2017-01-28 15:35:03 +0000

    hamburger css

--- js/script.js
+++ js/script.js
@@ -3,14 +3,14 @@ function get (id) { return document.getElementById(id); }
 function getTag(tag) { return Array.from(document.getElementsByTagName(tag)); }
 function getClass(c) { return Array.from(document.getElementsByClassName(c)); }
 
-get('main').appendChild(create('ul')).classList = 'YES';
-get('main').appendChild(create('ul'));
-get('main').appendChild(create('ul'));
-get('main').appendChild(create('ul'));
-get('main').appendChild(create('ul'));
-get('main').appendChild(create('ul'));
-
-getTag('ul').forEach(function (e, i) {
-  e.appendChild(create('li')).id = 'li' + i;
-  e.appendChild(create('li')).id = 'li' + i;
-});
+// get('main').appendChild(create('ul')).classList = 'YES';
+// get('main').appendChild(create('ul'));
+// get('main').appendChild(create('ul'));
+// get('main').appendChild(create('ul'));
+// get('main').appendChild(create('ul'));
+// get('main').appendChild(create('ul'));
+//
+// getTag('ul').forEach(function (e, i) {
+//   e.appendChild(create('li')).id = 'li' + i;
+//   e.appendChild(create('li')).id = 'li' + i;
+// });
