d11d2e930b42090531d93370c4b5af31279ddacb
finnhodgkin <finnhodgkin@gmail.com>
2017-02-07 20:26:24 +0000

    playing with arrays

--- react-tests/src/Hello.js
+++ react-tests/src/Hello.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
 
+
+
 class HelloWorld extends Component {
   constructor(props) {
     super(props);
@@ -16,13 +18,26 @@ class HelloWorld extends Component {
         date: new Date()
       });
     }, 1000);
+    const list = [];
+    fetch('https://gist.githubusercontent.com/finnhodgkin/4b12d304c3109fa337f09ec6d57200c3/raw/087a6ce15fb619085f7410759684df2ab170577a/cor-pun.json')
+    .then(blob => blob.json())
+    .then((data) => {
+      list.push(...data);
+      this.build(list);
+    });
   }
   componentWillUnmount() {
     clearInterval(this.timer);
   }
   edit() {
     this.setState({
-      test: 'test'
+      test: <b>hello</b>
+    });
+  }
+  build(data) {
+    const list = data.map((e) => <div key={e.name}>{e.name}</div>);
+    this.setState({
+      test: list
     });
   }
   render() {
